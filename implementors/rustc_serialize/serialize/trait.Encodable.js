73d29d556853ba1762e2bf830bd29c8cac326d14
Sam Giles <sam.e.giles@gmail.com>
2016-06-26 18:42:41 +0000

    Update documentation

--- implementors/rustc_serialize/serialize/trait.Encodable.js
+++ implementors/rustc_serialize/serialize/trait.Encodable.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['uuid'] = ["impl <a class='trait' href='rustc_serialize/serialize/trait.Encodable.html' title='rustc_serialize::serialize::Encodable'>Encodable</a> for <a class='struct' href='uuid/struct.Uuid.html' title='uuid::Uuid'>Uuid</a>",];
+implementors['uuid'] = ["impl <a class='trait' href='rustc_serialize/serialize/trait.Encodable.html' title='rustc_serialize::serialize::Encodable'>Encodable</a> for <a class='struct' href='uuid/struct.Uuid.html' title='uuid::Uuid'>Uuid</a>",];implementors["uuid"] = ["impl <a class='trait' href='rustc_serialize/serialize/trait.Encodable.html' title='rustc_serialize::serialize::Encodable'>Encodable</a> for <a class='struct' href='uuid/struct.Uuid.html' title='uuid::Uuid'>Uuid</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
